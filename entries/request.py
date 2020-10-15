d86e4efe3b757fe3556a56aba3935a3247307c25
Matt Logan <mlogan5212@gmail.com>
2020-10-14 20:33:15 -0500

    git single entry query

--- entries/request.py
+++ entries/request.py
@@ -25,3 +25,24 @@ def get_all_entries():
           entries.append(entry.__dict__)
 
     return json.dumps(entries)
+
+def get_single_entry(id):
+    with sqlite3.connect("./dailyjournal.db") as conn:
+        conn.row_factory = sqlite3.Row 
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+          e.id,
+          e.concept,
+          e.entry,
+          e.date,
+          e.moodId
+        FROM Entries e
+        WHERE e.id = ?
+        """, ( id, ))
+
+        data = db_cursor.fetchone()
+        entry = Entry(data['id'], data['concept'], data['entry'], data['date'], data['moodId'])
+
+        return json.dumps(entry.__dict__)
